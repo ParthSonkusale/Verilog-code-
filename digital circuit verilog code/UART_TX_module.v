2a642aedf6d7fc572946074baec396d4023db4ef
parthsonkusale <parthsonkusale27092006@gmail.com>
2025-08-22 10:21:57 -0700

    UART

--- digital circuit verilog code/UART_TX_module.v
+++ digital circuit verilog code/UART_TX_module.v
@@ -1,123 +1,119 @@
-module uart_tx(
+module uart_rx(
     input clk_3125,
-    input parity_type,tx_start,
-    input [7:0] data,
-    output reg tx, tx_done
-);
+    input rx,
+    output reg [7:0] rx_msg,
+    output reg rx_parity,
+    output reg rx_complete  
+    );
 
-parameter CLKS_PER_BIT = 14;
-parameter IDLE        = 3'd0;
-parameter START_BIT   = 3'd1;
-parameter DATA_BITS   = 3'd2;
-parameter PARITY_BIT  = 3'd3;
-parameter STOP_BIT    = 3'd4;
+//////////////////DO NOT MAKE ANY CHANGES ABOVE THIS LINE//////////////////
 
-reg [2:0] current_state = IDLE;
-reg [4:0] clk_count = 0;
-reg [2:0] bit_index = 0;
-reg parity_bit = 0;
-reg [7:0] tx_data = 8'd0;
+parameter CLKS_PER_BIT   = 14;
+
+parameter IDLE           = 3'b000;
+parameter RX_START_BIT   = 3'b001;
+parameter RX_DATA_BITS   = 3'b010;
+parameter RX_PARITY_BIT  = 3'b011;
+parameter RX_STOP_BIT    = 3'b100;
+
+
+reg [31:0] clk_count   = 0;
+reg [31:0] bit_index   = 0;
+reg [2:0] state       = IDLE;
+reg [7:0] data_byte   = 0;
+reg [7:0]parity_calc;
+reg x=1'b1;
+reg [4:0] counter;
+reg [7:0]rec_parity;
+ 
 
 initial begin
-    tx = 1;
-    tx_done = 0;
+    rx_msg = 0;
+    rx_parity     = 0;
+    rx_complete   = 0;
+   
+	 
 end
 
+
 always @(posedge clk_3125) begin
-    case (current_state)
+    case (state)
+	 
 
-    IDLE: begin
-        if (clk_count == 0) begin
-            tx <= 1;
-            tx_done <= 0;
-            bit_index <= 7;  
+        IDLE: begin
+            rx_complete = 0;
+            clk_count   = 0;
+            bit_index   = 0;
+            rec_parity=0;
+            if (rx == 0)  
+                state = RX_START_BIT;
         end
 
-        clk_count <= 0;
-
-        if (tx_start) begin
-            tx_data <= data;
-            parity_bit <= (^data) ^ parity_type;
-            tx <= 0;  
-            current_state <= START_BIT;
-        end
-    end
-
-    START_BIT: begin
-        if (clk_count == 0) begin
-            tx <= 0;  
-        end
-        else if (clk_count == CLKS_PER_BIT - 2) begin
-            tx <= tx_data[7];  
-        end
-
-        if (clk_count == CLKS_PER_BIT) begin
-            clk_count <= 0;
-            current_state <= DATA_BITS;
-        end else begin
-            clk_count <= clk_count + 1;
-        end
-    end
-
-    DATA_BITS: begin
-        if (clk_count == 0) begin
-            tx <= tx_data[bit_index];  
-        end
-        else if (clk_count == CLKS_PER_BIT - 2) begin
-            if (bit_index > 0)
-                tx <= tx_data[bit_index - 1];  
-            else
-                tx <= parity_bit; 
-        end
-
-        if (clk_count == CLKS_PER_BIT - 1) begin
-            clk_count <= 0;
-            if (bit_index > 0)
-                bit_index <= bit_index - 1;
-            else begin
-                bit_index <= 0;
-                current_state <= PARITY_BIT;
+        RX_START_BIT: begin
+            if (clk_count == (CLKS_PER_BIT - 1)/2) begin
+                if (rx == 0) begin  
+                    clk_count = 0;
+                    state     = RX_DATA_BITS;
+                end else begin
+                    state = IDLE;  
+                end
+            end else begin
+                clk_count = clk_count + 1;
             end
-        end else begin
-            clk_count <= clk_count + 1;
         end
+
+        RX_DATA_BITS: begin
+            if (clk_count < CLKS_PER_BIT -1) begin
+                
+                clk_count = clk_count + 1;
+           end else begin
+			  data_byte[7-bit_index] <= rx;
+                clk_count = 0;
+                if (bit_index < 7)
+                    bit_index = bit_index + 1;
+                else begin
+                    bit_index = 0;
+						  
+                    state     = RX_PARITY_BIT;
+                end
+            end
+        end
+
+       RX_PARITY_BIT: begin
+
+    if (clk_count < CLKS_PER_BIT - 1)begin
+        clk_count = clk_count + 1;
+    end else begin
+rec_parity=rx;
+	     parity_calc=^data_byte;
+		  
+        
+		  clk_count = 0;
+        state = RX_STOP_BIT;
     end
+end
 
-    PARITY_BIT: begin
-        if (clk_count == 0) begin
-            tx <= parity_bit;
-        end
-        else if (clk_count == CLKS_PER_BIT - 2) begin
-            tx <= 1; 
-        end
 
-        if (clk_count == CLKS_PER_BIT - 1) begin
-            clk_count <= 0;
-            current_state <= STOP_BIT;
-        end else begin
-            clk_count <= clk_count + 1;
-        end
-    end
+        RX_STOP_BIT: begin
+		 if(clk_count < CLKS_PER_BIT + 5 + x) begin
+                clk_count = clk_count + 1;
+            end else begin
+rx_parity=(rec_parity==parity_calc)?(rec_parity):8'h3F;
+                
+                rx_msg=(rec_parity==parity_calc)?(data_byte):8'h3F;
+					 clk_count   = 0;
+                rx_complete = 1;
+                x=0;
+                
 
-    STOP_BIT: begin
-        if (clk_count == 0) begin
-            tx <= 1;
+                state       = IDLE;
+            end
         end
-
-        if (clk_count == CLKS_PER_BIT - 3) begin
-            tx_done <= 1;  
-        end
-
-        if (clk_count == CLKS_PER_BIT - 2) begin
-            clk_count <= 0;
-            tx_done <= 0;
-            tx <= 1;
-            current_state <= IDLE;
-        end else begin
-            clk_count <= clk_count + 1;
-        end
-    end
 
     endcase
 end
+
+//////////////////DO NOT MAKE ANY CHANGES BELOW THIS LINE//////////////////
+
+
 endmodule
